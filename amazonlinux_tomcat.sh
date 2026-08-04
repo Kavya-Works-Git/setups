6749ab4975f84b1c19ddbd06520143f04c2d9825
kavya <naidukavya18@gmail.com>
2026-08-04 07:12:34 +0530

    Create amazonlinux_tomcat.sh

---
+++ amazonlinux_tomcat.sh
@@ -0,0 +1,35 @@
+# 1. Install Java (Amazon Linux uses yum/dnf)
+yum update -y
+yum install -y java-21-amazon-corretto
+
+# 2. Set JAVA_HOME
+echo 'export JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto' >> ~/.bashrc
+source ~/.bashrc
+
+# 3. Download and extract Tomcat
+cd ~
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.120/bin/apache-tomcat-9.0.120.tar.gz
+tar -zxvf apache-tomcat-9.0.120.tar.gz
+
+# 4. Create manager user (clean rewrite)
+cat > apache-tomcat-9.0.120/conf/tomcat-users.xml << 'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<tomcat-users xmlns="http://tomcat.apache.org/xml"
+              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
+              version="1.0">
+<role rolename="manager-gui"/>
+<role rolename="manager-script"/>
+<user username="tomcat" password="admin@123" roles="manager-gui,manager-script"/>
+</tomcat-users>
+EOF
+
+# 5. Allow remote access to Manager and Host Manager apps
+sed -i '/<Valve/,/\/>/d' apache-tomcat-9.0.120/webapps/manager/META-INF/context.xml
+sed -i '/<Valve/,/\/>/d' apache-tomcat-9.0.120/webapps/host-manager/META-INF/context.xml
+
+# 6. Start Tomcat
+sh apache-tomcat-9.0.120/bin/startup.sh
+
+# 7. Confirm it started
+tail -30 apache-tomcat-9.0.120/logs/catalina.out
