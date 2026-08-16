af20a4df43de2cd9b247d21563907ae9d32df9f9
kavya <naidukavya18@gmail.com>
2026-08-16 14:08:30 +0530

    Update amazonlinux_maven.sh

--- amazonlinux_maven.sh
+++ amazonlinux_maven.sh
@@ -1,7 +1,10 @@
+sudo dnf update -y
 sudo dnf install java-17-amazon-corretto -y
-readlink -f $(which java)
-export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64
-export PATH=$JAVA_HOME/bin:$PATH
+
+# Set JAVA_HOME and PATH
 echo 'export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64' | sudo tee /etc/profile.d/java.sh
 echo 'export PATH=$JAVA_HOME/bin:$PATH' | sudo tee -a /etc/profile.d/java.sh
 source /etc/profile.d/java.sh
+
+# Verify
+java -version
